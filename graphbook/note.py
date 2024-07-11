a1d86bcaa332726ab80e39ff90d1ced17568ab48
Sammy Franklin <sammyfranklin@yahoo.com>
2024-07-11 12:34:05 -0700

    no need for annotate method, users can just create dicts

--- graphbook/note.py
+++ graphbook/note.py
@@ -1,38 +1,4 @@
 import json
-from .utils import is_batchable
-from typing import Iterable
-from torch import Tensor
-
-# class DataItem:
-#     """
-#     Data structure containing the text, audio, image, or video coupled with its annotation.
-
-#     Args:
-#         item (str): A path to media, string of text, or the actual data item to store
-#         type (str): Optional string to specify the type of data this is
-#         annotation (dict): A dictionary of annotations for this item
-
-#     Example:
-#         .. highlight:: python
-#         .. code-block:: python
-
-#             d = DataItem("path/to/image.jpg", "image", {"prediction": "dog"})
-#     """
-
-#     def __init__(self, item, type=None, annotation={}):
-#         self.item = item
-#         self.type = type
-#         self.annotation = annotation
-
-#     def __str__(self):
-#         return f"{'('+self.type+')' if self.type else ''}Item {self.item}: Annotations: {self.annotation}"
-
-#     def json(self):
-#         """
-#         Returns DataItem into a serialized JSON format
-#         """
-#         return {"item": self.item, "type": self.type, "annotation": self.annotation}
-
 
 class Note:
     """
@@ -60,7 +26,7 @@ class Note:
             value (str): The value of the item
         """
         self.items[key] = value
-        
+
     def __getitem__(self, key: str):
         """
         A convenient function to retrieve an item
@@ -69,28 +35,6 @@ class Note:
             item_key (str): The item key to retrieve from
         """
         return self.items.get(key)
-    
-    def annotate(self, key: str, index: int, annotation: str):
-        """
-        Should only use if item at key is an iterable or Tensor. Otherwise, use __setitem__. Annotates an item in the note.
-        The string annotation will be stored in the Note as an item with key being *"{item_key}_annotation"* at index.
-
-        Args:
-            item_key (str): The item key to annotate
-            annotation (dict): The annotation to add to the item
-        """
-        to_be_annotated = self.items.get(key)
-        if not to_be_annotated:
-            return None
-        if not is_batchable(to_be_annotated):
-            return None
-        annotation_key = f"{key}_annotation"
-        annotations = self.items.get(annotation_key)
-        n = len(to_be_annotated)
-        if not annotations:
-            self.items[annotation_key] = [None for _ in range(n)]
-        
-        self.items[annotation_key][index] = annotation
 
     def __str__(self):
         return json.dumps(self.items, indent=2)
