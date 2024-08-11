3069fd334ee32d072023464c2d567146d9c0060b
Richard Franklin <rsamfranklin@gmail.com>
2024-08-10 22:22:12 -0700

    creating remote read/write steps

--- graphbook/steps/remote.py
+++ graphbook/steps/remote.py
@@ -1,35 +1,32 @@
-from graphbook.steps.base import Step, AsyncStep
+from graphbook.steps.base import Step
+from graphbook.note import Note
+import queue
+
 
 class RemoteReadStep(Step):
     def __init__(self, id, logger):
         super().__init__(id, logger)
-        self._remote = None
+        self._queue = queue.Queue()
 
-    def set_remote(self, remote):
-        self._remote = remote
+    def enqueue_note(self, step_id: str, note: Note):
+        self._queue.put((step_id, note))
 
-    async def run(self, inputs):
-        if self._remote is None:
-            raise ValueError("Remote is not set")
-        return await self._remote.run(inputs)
+    def __call__(self, note: Note):
+        if self._queue.empty():
+            return {}
+        step_id, note = self._queue.get(False)
+        return {step_id: note}
 
-    async def close(self):
-        if self._remote is not None:
-            await self._remote.close()
 
 class RemoteWriteStep(Step):
     def __init__(self, id, logger):
         super().__init__(id, logger)
-        self._remote = None
+        self._queue = queue.Queue()
 
-    def set_remote(self, remote):
-        self._remote = remote
+    def dequeue_note(self) -> Note | None:
+        if self._queue.empty():
+            return None
+        return self._queue.get(False)
 
-    async def run(self, inputs):
-        if self._remote is None:
-            raise ValueError("Remote is not set")
-        return await self._remote.run(inputs)
-
-    async def close(self):
-        if self._remote is not None:
-            await self._remote.close()
+    def __call__(self, note: Note):
+        self._queue.put(note)
