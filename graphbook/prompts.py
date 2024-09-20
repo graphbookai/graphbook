3362944e252928d78f06cf7b92ffb9dc1e5da3a4
Richard Franklin <rsamfranklin@gmail.com>
2024-09-19 18:04:25 -0700

    cleaning up

--- graphbook/prompts.py
+++ graphbook/prompts.py
@@ -2,22 +2,31 @@ from typing import Any
 from .note import Note
 from .utils import transform_json_log
 
+
 def none():
     return {"type": None}
 
-def prompt(note: Note, msg: str, show_images: bool=False, default: Any=""):
+
+def prompt(note: Note, msg: str, show_images: bool = False, default: Any = ""):
     return {
         "note": transform_json_log(note),
         "msg": msg,
         "show_images": show_images,
-        "def": default
+        "def": default,
     }
 
-def bool_prompt(note: Note, msg: str="Continue?", style: str="yes/no", default: bool=False, show_images: bool=False):
+
+def bool_prompt(
+    note: Note,
+    *,
+    msg: str = "Continue?",
+    style: str = "yes/no",
+    default: bool = False,
+    show_images: bool = False,
+    pause: bool = True,
+):
     default = "Yes" if default else "No"
     p = prompt(note, msg, default=default, show_images=show_images)
     p["type"] = "bool"
-    p["options"] = {
-        "style": style
-    }
+    p["options"] = {"style": style}
     return p
